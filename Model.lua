57419b41c7e8c5491d5d0520fe1558f35e61cf37
= <jochem-brouwer@users.noreply.github.com>
2017-06-28 20:13:27 +0200

    less code is better

--- Model.lua
+++ Model.lua
@@ -39,29 +39,45 @@ function Model:Run(PList, SweepMode, Options)
 	local DataOut = {};
 
 	local ANIM = Options and Options.Anim;
+	local SAVE = Options and Options.Save;
+	local PLOT = Options and Options.Plot;
 
-	if SweepMode == "Cycle" then 
+
 		for i = 1, #PList[index] do 
 			-- Set vars.
 			for ParamName, Params in pairs(PList) do 
 				self.Lattice[ParamName] = Params[i];
 
 			end 
-			local rep = true;
-			while rep do 
-				rep=false;
-			-- Sweep over grain.
-				for _, Grain in pairs(self.Lattice.Grains) do 
-					--print(self.Lattice:GetDeltaU(Grain))
-					if self.Lattice:GetDeltaU(Grain) < 0 then 
-						self.Lattice:FlipSpin(Grain);
-						rep=true;
+			if SweepMode == "Cycle" then 
+				local rep = true;
+				while rep do 
+					rep=false;
+				-- Sweep over grain.
+					for _, Grain in pairs(self.Lattice.Grains) do 
+						--print(self.Lattice:GetDeltaU(Grain))
+						if self.Lattice:GetDeltaU(Grain) < 0 then 
+							self.Lattice:FlipSpin(Grain);
+							rep=true;
+						end 
+					end
+				end
+			elseif SweepMode == "Metropolis" then 
+				for Sweep = 1,Sweeps do 
+					-- Pick a random grain; 
+					local Grain = self.Lattice:GetRandomLatticeSite();
+					local dU = self.Lattice:GetDeltaU(Grain);
+			--		print(dU, dU/(self.k*self.Lattice.Temperature), math.exp(-dU/(self.k*self.Lattice.Temperature)))
+					if dU <= 0 then 
+						self.Lattice:FlipSpin(Grain) 
+					elseif math.exp(-dU/(self.k*self.Lattice.Temperature)) > math.random() then 
+						self.Lattice:FlipSpin(Grain)
 					end 
 				end
-			end
+			end 
 --			self.Lattice:Show() 
 --			print(self.Lattice:GetM(), PList.ExternalField[i])
-			if ANIM then 
+			if ANIM or SAVE then 
 				self.Lattice:Dump("tmp.lat")
 			end 
 			local Measure = self:Measure(self.Lattice);
@@ -86,7 +102,7 @@ function Model:Run(PList, SweepMode, Options)
 			self.Lattice:ToAnim("tmp.lat", "out.gif",4,5,1)
 		end
 		-- Plot this data.
-
+		if PLOT then 
 		for xLabel, Contents in pairs(DataOut) do 
 			for yLabel, Data in pairs(Contents) do 
 				local NewPlot = Plotter:New();
@@ -118,110 +134,9 @@ function Model:Run(PList, SweepMode, Options)
 				NewPlot:Plot(yLabel.."vs"..xLabel..".png")
 			end 
 		end 
-
-	elseif SweepMode == "Metropolis" then 
-		local SweepOptions = Options or {};
-		-- One full sweep per setting is default.
-		local Sweeps = SweepOptions.Sweeps or self.Lattice.x*self.Lattice.y*self.Lattice.z;
-		local k = self.k;
-		for i = 1, #PList[index] do 
-			-- Set vars.
-			for ParamName, Params in pairs(PList) do 
-				self.Lattice[ParamName] = Params[i];
-			end 
-			local rep = true;
-	--[[		while rep do 
-				rep=false;
-			-- Sweep over grain.
-				for _, Grain in pairs(self.Lattice.Grains) do 
-					--print(self.Lattice:GetDeltaU(Grain))
-					if self.Lattice:GetDeltaU(Grain) < 0 then 
-						self.Lattice:FlipSpin(Grain);
-						rep=true;
-					end 
-				end
-			end--]]
-
-			for Sweep = 1,Sweeps do 
-				-- Pick a random grain; 
-				local Grain = self.Lattice:GetRandomLatticeSite();
-				local dU = self.Lattice:GetDeltaU(Grain);
-		--		print(dU, dU/(self.k*self.Lattice.Temperature), math.exp(-dU/(self.k*self.Lattice.Temperature)))
-				if dU <= 0 then 
-					self.Lattice:FlipSpin(Grain) 
-				elseif math.exp(-dU/(self.k*self.Lattice.Temperature)) > math.random() then 
-					self.Lattice:FlipSpin(Grain)
-				end 
-			end 
-
-			if ANIM then 
-				self.Lattice:Dump("tmp.lat")
-			end 
-
-
-		--	print(self.Lattice.ExternalField)
-		--	self.Lattice:Show() 
---			print(self.Lattice:GetM(), PList.ExternalField[i])
---			self.Lattice:Dump("tmp.lat")
-		
-			local Measure = self:Measure(self.Lattice);
-
-			for ParamName, Data in pairs(PList) do 
-				if not DataOut[ParamName]then 
-					DataOut[ParamName] = {};
-				end 
-				local xValue = Data[i]; 
-
-				for index, yValue in pairs(Measure) do 
-					if not (DataOut[ParamName][index]) then 
-						DataOut[ParamName][index] = {x = {}, y = {}};
-					end 
-					table.insert(DataOut[ParamName][index].x, xValue);
-					table.insert(DataOut[ParamName][index].y, yValue);
-				end 
-			end 
-
-		end
-		
-
-		for xLabel, Contents in pairs(DataOut) do 
-			for yLabel, Data in pairs(Contents) do 
-				local NewPlot = Plotter:New();
-				NewPlot:Set("xlabel", xlabel) -- Create a new plot. 
-				NewPlot:Set("ylabel", ylabel);
-				local Data = Data;
-				if yLabel == "M" and xLabel == "ExternalField" then 
-					local new = {};
-					new.x = {};
-					new.y = {};
-					local min_x = mint(Data.x);
-					local max_x = maxt(Data.x);
-					for ind, val in pairs(Data.x) do 
-						table.insert(new.x, (val - min_x)/(max_x - min_x));
-					end 
-					local min_y = mint(Data.y);
-					local max_y = maxt(Data.y);
-					for ind, val in pairs(Data.y) do 
-						table.insert(new.y, (val - min_y)/(max_y - min_y));
-					end 
-
-					NewPlot:SetData("xdata", new.x,true);
-					NewPlot:SetData("ydata", new.y);
-				else 
-					NewPlot:SetData("xdata", Data.x,true);
-					NewPlot:SetData("ydata", Data.y);
-				end 
-			--	NewPlot:SetData("title", yLabel .. " vs " .. xLabel)
-				NewPlot:Plot(yLabel.."vs"..xLabel..".png")
-			end 
 		end 
+		return DataOut;
 
-		if ANIM then 
-			self.Lattice:ToAnim("tmp.lat", "out.gif",4,5,1)
-		end
-
-
-	end
 
 
 end 
